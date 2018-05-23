7c61e63a2214922779115d115d40ea8cbdd0a141
Diane Blackwood <Diane.Blackwood@gmail.com>
2018-05-23 01:14:31 -0400

    added details/summary based collapsible list; added outline of population page

---
+++ avidaEdEco.js
@@ -0,0 +1,21 @@
+//For now this is just for comments
+/*
+ 
+
+//-----------------------------------------------------------------------------
+//   Methods to do a collapsible list
+//-----------------------------------------------------------------------------
+
+First implemented: https://codepen.io/huange/pen/pJqEMj?editors=1100
+  this uses the checkbox input method and modifies it. 
+
+Next implemented: https://stackoverflow.com/questions/24977965/collapsible-lists-using-html-and-css
+  uses <details> and <summary> tags
+
+  Examples that use javascript
+  https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion
+  https://www.w3schools.com/howto/howto_js_accordion.asp
+  
+
+
+*/
